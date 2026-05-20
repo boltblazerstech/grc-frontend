958b699d1f4a04f2d37836a7c7b40c0313a97027
CandidateMaster2002 <kickmegreen@gmail.com>
2026-05-20 19:14:24 +0530

    chore: update API proxy targets to production Cloud Run instance and remove base directory configuration

--- vite.config.js
+++ vite.config.js
@@ -7,8 +7,8 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        // target: 'http://65.2.170.175:8080',
-        target: 'http://localhost:8080',
+        target: 'https://grc-backend-135177300761.asia-south1.run.app',
+        // target: 'http://localhost:8080',
         changeOrigin: true,
         secure: false,
       }
